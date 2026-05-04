716df390ffa09ee74b0892850d5d1a21f4f564e4
juankzaba <72588538+juankzaba@users.noreply.github.com>
2026-05-04 18:40:25 -0500

    Mejora los nombres de variables para mayor claridad

---
+++ Variables/nombres_variables.py
@@ -0,0 +1,18 @@
+#Programa: Regristro de explorador espacial
+
+#Asignar valores a las variables
+
+nombre_explorador = 'Luna Vega'
+planeta_origen = 'Marte'
+edad_explorador = 29
+misiones_completadas = 4
+nivel_energia = 87.5
+
+#Imprimir los valores
+
+print('==REGISTRO ESPACIAL==')
+print('Nombre:', nombre_explorador)
+print('Planeta:',planeta_origen)
+print('Edad:',edad_explorador)
+print('Misiones Completadas:',misiones_completadas)
+print('Nivel Energia:', nivel_energia)

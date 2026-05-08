09be916b14bbff259e6472c5f4b14c7acf0d4664
juankzaba <juankzaba34@gmail.com>
2026-05-08 16:50:09 -0500

    Buscar indices en cadenas

---
+++ Cadenas/multiplicacion_cadenas.py
@@ -0,0 +1,19 @@
+# Programa: Multiplicación de Cadenas
+
+# 1. Crear un separador visual
+linea = '='*20
+print(linea)
+# Salida: ====================
+
+#2. Indentación simple
+nivel = 2
+sangria = '    '*nivel
+print(sangria + 'definición_bloque_codigo')#salida:       definición_bloque_codigo
+
+# 3. Patrones simples
+print('1'*5)# '11111' (texto, no numero)
+
+#4 error comun (Comentado para evitar crash)
+#print('Hola'*2.5) # TypeError!
+
+

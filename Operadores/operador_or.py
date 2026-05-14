c74e949887b8936769924fb6d8c4e19f55f5a0b3
juankzaba <juankzaba34@gmail.com>
2026-05-14 15:44:55 -0500

    Ejemplo de operador OR

---
+++ Operadores/operador_or.py
@@ -0,0 +1,6 @@
+print('*** Operador lógico or ***')
+condicion1 = False
+condicion2 = False
+# El operador or regresa True si cualquiera de los operandos es True
+resultado = condicion1 or condicion2
+print(f"Resultado {condicion1} or {condicion2}: {resultado}")

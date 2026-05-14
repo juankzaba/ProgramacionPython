ece5fec357f78926e3730e3a26a718a3011c52f1
juankzaba <juankzaba34@gmail.com>
2026-05-14 11:39:18 -0500

    Ejemplos de opeadores de asignación

---
+++ Operadores/operadores_asignacion_compuestos.py
@@ -0,0 +1,23 @@
+print('*** Operadores Asignación Compuesto **')
+a, b = 10, 15
+print(f"Valor inicial a: {a}, b:{b}")
+
+# Operador compuesto de suma +=
+
+a += b # a = a + b
+print(f"Operador a += b es: {a}")
+
+# Operador compuesto de resta -=
+a = 10 # Reiniciamos la variable a
+a -= b # a = a - b
+print(f"Operador a -= b es: {a}")
+
+# Operador compuesto de multiplicación -*
+a = 10 # Reiniciamos el valor de a
+a *= b # a = a * b
+print(f"Operador a *= b es: {a}")
+
+ # Operador compuesto de División -*
+a = 10 # Reiniciamos el valor de a
+a /= b # a = a / b
+print(f"Operador a /= b es: {a:2.f}")

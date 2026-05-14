6d8b4a79268db661cf21213e060105f003028ae7
juankzaba <juankzaba34@gmail.com>
2026-05-13 23:39:11 -0500

    Operaciones aritméticas

---
+++ Operadores/operadores_aritmeticos.py
@@ -0,0 +1,32 @@
+# Operadores aritméticos
+
+a = 10
+b = 3
+
+# Suma +
+suma = a + b
+print(f"Suma: {suma}")
+
+# Resta -
+resta = a - b
+print(f"Resta: {resta}")
+
+# Multiplicación *
+multiplicacion = a * b
+print(f"Multiplicación: {multiplicacion}")
+
+# División
+division = a / b # Retorna un tipo float
+print(f"División: {division:.2f}")
+
+# División entero //
+division_entero = a // b
+print(f"División entera: {division_entero}")
+
+# Modulo %
+modulo = a % b
+print(f"Residuo de la división: {modulo}")
+
+# Exponente (potencia) **
+exponente = a ** b # 10 elevado a la 3 0 10*10*10= 1000
+print(f"Exponente: {exponente}")

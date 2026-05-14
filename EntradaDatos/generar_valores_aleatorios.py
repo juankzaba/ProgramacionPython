14e219bd0fc01f4f90d4779c9acf811556233d8d
juankzaba <juankzaba34@gmail.com>
2026-05-13 19:13:18 -0500

    Función Random

---
+++ EntradaDatos/generar_valores_aleatorios.py
@@ -0,0 +1,10 @@
+# Valores aleatorios con la función randint
+from random import randint
+
+# Generar un numero aleatorio entre 1 y 10
+numero = randint(1,10)
+print(f"Numero aleatorio entr 1 y 10: {numero}")
+
+# Simular un dado de 6 caras
+dado = randint(1,6)
+print(f"Resultado de lanzar el dado: {dado}")

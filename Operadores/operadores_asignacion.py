bf3daaec300f1f3fd3167f64e7810c3c1816570f
juankzaba <juankzaba34@gmail.com>
2026-05-14 11:13:52 -0500

    Ejemplos de asignación

--- Operadores/operadores_asignacion.py
+++ Operadores/operadores_asignacion.py
@@ -5,3 +5,22 @@ numero = 10
 print(f"Valor de numero: {numero}")
 cadena = 'Saludos desde Python'
 print(f"Valor de la cadena: {cadena}")
+
+# Asignación Multiple
+x,y,z = 5, 'Hola', -9.26
+print(f"Valor de x = {x}, y = {y}, z = {z}")
+
+# Asignación Encadenada
+a = b = c = 10
+print(f"Valor de x = {a}, b = {b}, c = {c}")
+
+# Intercambio de valores de una variable, sin utilizar variables temporales
+x, y = 5, 10
+print(f"Valores iniciales: x = {x}, y = {y}")
+# Aplicando el concepto de asignación multiple, intercambiamos valores
+x, y = y, x
+print(f"Invertir los valores: x = {x}, y = {y}")
+
+# Recibir multiples valores de la entrada del usuario
+nombre, apellido = input("Ingresa tun nombre y apellido separados por coma: ").split(',')
+print(f"Nombre: {nombre.strip()}, Apellido: {apellido.strip()}")

6024c99c92f8f9d5598f044609ff114f156da302
juankzaba <juankzaba34@gmail.com>
2026-05-15 07:41:22 -0500

    Validar si un número está dentro de un rango

---
+++ Operadores/dentro_rango_not.py
@@ -0,0 +1,12 @@
+# Revisar si una variable se encuentr dentro de rango 1 y 10
+from xlwings import RangeColumns
+
+dato = int(input("Proporciona un dato entero: "))
+
+# Revisamos si está dentro de Rango
+esta_dentro_de_rango = 1 <= dato <= 10
+print(f"Variable está dentro de rango (entre 1 y 10)?: {esta_dentro_de_rango}")
+
+# Revisamos lo lógica inversa, si el dato está fuera de esta_dentro_de_rango
+esta_fuera_rango = not(1 <= dato <= 10)
+print(f"Variable está fuera de rango (entre 1 y 10)?: {esta_fuera_rango}")

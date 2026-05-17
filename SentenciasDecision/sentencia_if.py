90b083c358e1a8a1b62de9ab0ed99426728c09fa
juankzaba <juankzaba34@gmail.com>
2026-05-16 22:36:12 -0500

    Conocer si se es menor o mayor de edad

--- SentenciasDecision/sentencia_if.py
+++ SentenciasDecision/sentencia_if.py
@@ -5,4 +5,9 @@ edad = int(input("Cuántos años tienes?: "))
 
 if edad >= MAYOR_EDAD:
     print(f"Eres mayor de edad. Tienes {edad} años")
+elif 13 <= edad < 18:
+    print(f"Eres un adolescente. Tienes {edad} años")
+else:
+    print(f"Eres un niño. Tienes {edad} años")
+
 
